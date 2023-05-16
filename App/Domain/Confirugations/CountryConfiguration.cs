0375461e65d3f5446a44a4cf5ddd2c38c384e87b
mubarizra@code.edu.az <mubarizra@code.edu.az>
2023-05-17 01:56:37 +0400

    ApiFirstLesson

---
+++ App/Domain/Confirugations/CountryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Confirugations
+{
+    public class CountryConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.Property(m => m.Name).IsRequired().HasMaxLength(100);
+            builder.Property(m => m.SoftDelete).IsRequired().HasDefaultValue(false);
+            builder.Property(m => m.CreateAt).IsRequired().HasDefaultValue(DateTime.UtcNow);
+        }
+    }
+}
---
+++ App/Repository/Data/AppDbContext.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Confirugations;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+
+
+            modelBuilder.Entity<Employee>().HasData(
+                  new Employee
+                  {
+                      Id = 1,
+                      FullName = "Ceyhun Yusifli",
+                      Address = "Buzovna",
+                      Age = 23,
+                  },
+
+                   new Employee
+                   {
+                       Id = 2,
+                       FullName = "Mubariz Agayev",
+                       Address = "Nizami",
+                       Age = 18,
+                   },
+
+                    new Employee
+                    {
+                        Id = 3,
+                        FullName = "Shaig Kazimova",
+                        Address = "Sedmoy",
+                        Age = 25,
+                    });
+
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Azerbaycan"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Turkiye"
+                }, new Country
+                {
+                    Id = 3,
+                    Name = "Gurcustan"
+                });
+
+        }
+    }
+}
